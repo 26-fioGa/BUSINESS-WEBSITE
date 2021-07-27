9f727c292b21c22569f2ae9835548f5375e5e392
fiorellaGamarra <fiorella.gamarra@tecsup.edu.pe>
2021-07-27 00:52:54 -0500

    avance vistas de minerias .2

--- src/pages/services/Planta.js
+++ src/pages/services/Planta.js
@@ -20,6 +20,25 @@ const Planta = () => {
                     </div>
                 </div>
                 <div class="container-fluid pt-5">
+                    <div className="row ml-3 mr-3 pl-2 pr-2 pb-3">
+                        <div className="col-lg-12">
+                            <p>PROYINTER inmobiliaria se enfoca en promover y administrar proyectos de ingeniería en cuanto a infraestructura, tales como:</p>
+                            <br />
+                            <p>Elaboración de diseños nuevos para condominios, departamentos, casas, etc. Así como también ampliación, modificación o remodelación de cualquier edificación.</p>
+                            <ul>
+                                <li>Planos civiles</li>
+                                <li>Planos arquitectónicos</li>
+                                <li>Ampliaciones y remodelaciones en edificaciones</li>
+                                <li>Animación 3D</li>
+                                <li>Diseño de interiores
+                                    <ul>
+                                        <li>Acabados</li>
+                                        <li>Diseños</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                     <div className="row">
                         <div className="col-lg-12 text-center mt-3 p-3 note">
                             <h6>ASESORÍA INTEGRAL EN TODO EL PROCESO</h6>
